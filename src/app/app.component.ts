b2877ad7fad86022532df20883e6cad7ad595a58
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2018-11-05 09:06:09 -0200

    Novos recursos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component } from '@angular/core';
-import { PhotoService } from './photos/photo/photo.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
-  photos: Object[] = [];
-
-  constructor(pS: PhotoService) {
-
-    pS
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos);
-
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoModule } from './photos/photos.modules';
-import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+    BrowserModule,    
     PhotoModule,
-    HttpClientModule
+    AppRoutingModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
