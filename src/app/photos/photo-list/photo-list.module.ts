070a94e4337acefc3942c4019557cee5f7f83a52
TonyAbrunhosa <37698049+TonyAbrunhosa@users.noreply.github.com>
2018-11-09 00:05:14 -0200

    componentes

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -5,17 +5,23 @@ import { FilterByDescription } from './filter-by-description.pipe';
 import { CommonModule } from '@angular/common';
 import { MyPhotoComponent } from './my-photo/my-photo.component';
 import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from 'src/app/shared/components/card/card.module';
+import { SearchComponent } from './search/search.component';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 
 @NgModule({
     declarations: [
         PhotoListComponent,
         MyPhotoComponent,
         LoadButtonComponent,
-        FilterByDescription
+        FilterByDescription,
+        SearchComponent,
     ],
     imports: [ 
         CommonModule,
-        PhotoModule
+        PhotoModule,
+        CardModule,
+        DarkenOnHoverModule
      ]
 })
 export class PhotoListModule {}
